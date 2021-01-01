c212d7890f3ed55ae70a486be536db9a9a82fab0
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-01-01 23:59:30 +0800

    Create java_sol.java

---
+++ java_sol.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static final Reader in = new Reader();
+	static final PrintWriter out = new PrintWriter(System.out);
+
+	public static void main(String[] args) {
+		int t=in.nextInt();
+//		int t=1;
+		for(int i=1; i<=t; ++i)
+			new Solver();
+		out.close();
+	}
+	
+	static class Solver {
+		Solver() {
+			int n=in.nextInt();
+			HashSet<Integer> s = new HashSet<Integer>();
+			for(int i=0; i<n; ++i) {
+				int a=in.nextInt();
+				if(a>0)
+					s.add(a);
+			}
+			out.println(s.size());
+		}
+	}
+	
+	static class Reader {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		String next() {
+			while(st==null||!st.hasMoreTokens()) {
+				try {
+					st=new StringTokenizer(in.readLine());
+				} catch(Exception e) {}
+			}	
+			return st.nextToken();
+		}
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+	}
+	
+}
